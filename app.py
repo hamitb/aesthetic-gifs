e83947545eb0a671dbf4f5f8463dadfdc3e7d0a7
Hamit Burak Emre <hamitburakemre@gmail.com>
2018-11-27 09:47:34 +0300

    Add defualt gif to show in case of exception

--- app.py
+++ app.py
@@ -4,23 +4,29 @@ import utils
 
 wd = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(wd, "deps/"))
-print(sys.path)
 
 import giphy_client
+from giphy_client.rest import ApiException
 
 def handler(event, context):
     api_key = os.environ.get('giphy_api_key')
     gif_width = int(os.environ.get('gif_width', '800'))
     tag = os.environ.get('giphy_tag', 'vaporwave')
     api_intance = giphy_client.DefaultApi()
-    random_gif = api_intance.gifs_random_get(api_key, tag=tag)
 
-    gif_id = random_gif.data.id
-    width = int(random_gif.data.image_width)
-    scale_ratio = gif_width / width
+    try:
+        random_gif = api_intance.gifs_random_get(api_key, tag=tag)
+        gif_id = random_gif.data.id
+        width = int(random_gif.data.image_width)
+        scale_ratio = gif_width / width
+        
+        width = int(width * scale_ratio)
+        height = int(int(random_gif.data.image_height) * scale_ratio)
+    except ApiException:
+        gif_id = os.environ.get('default_gif_id', '3oz8xZ1t7hxPfe19M4')
+        width = gif_width
+        height = gif_width
 
-    width = int(width * scale_ratio)
-    height = int(int(random_gif.data.image_height) * scale_ratio)
     embed_url = f'https://giphy.com/embed/{gif_id}'
     
     return {
